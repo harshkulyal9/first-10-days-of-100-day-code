a76ef6cd60afb66b588d5d98f471542d380cefd5
harshkulyal9 <harshvardhankulyal0@gmail.com>
2025-08-26 23:03:46 +0530

    Add files via upload

---
+++ positivenegativeinteger.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+int n;
+printf("enter the number:");
+scanf("%d",&n);
+if(n<0){
+    printf("negative number");
+}
+if(n>0){
+    printf("positive number");
+}
+if(n==0){
+    printf("zero");
+}
+return 0;
+
+}
