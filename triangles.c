2bf04123cf46dc5ce99c769a720161229806b537
harshkulyal9 <harshvardhankulyal0@gmail.com>
2025-08-30 23:08:32 +0530

    Add files via upload

---
+++ triangles.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    float a;
+    printf("enter the value of a:");
+    scanf("%f",&a);
+     float b;
+    printf("enter the value of b:");
+    scanf("%f",&b);
+     float c;
+    printf("enter the value of c:");
+    scanf("%f",&c);
+    if(a==b && a==c && b==c){
+        printf("equilateral triangle");
+    }
+    else if(a==b && a!=c || b==c && b!=a || c==a && c!=b){
+        printf("issocles triangle");
+    }
+    else{
+        printf("scalene triangle");
+    }
+    return 0;
+}
